241046d098de00ee19db5e69591d161ec4b563b6
nacholupi <nacholupi@gmail.com>
2022-05-19 15:10:44 -0300

    94. Binary Tree Inorder Traversal iterative

--- 10-tree/94_inorder.go
+++ 10-tree/94_inorder.go
@@ -16,3 +16,49 @@ func traversal(root *TreeNode, r *[]int) {
 	*r = append(*r, root.Val)
 	traversal(root.Right, r)
 }
+
+type stack []*TreeNode
+
+func (s *stack) push(r *TreeNode) {
+	*s = append(*s, r)
+}
+
+func (s *stack) isEmpty() bool {
+	return len(*s) == 0
+}
+
+func (s *stack) peek() *TreeNode {
+	i := len(*s) - 1
+	e := (*s)[i]
+	return e
+}
+
+func (s *stack) pop() *TreeNode {
+	i := len(*s) - 1
+	e := (*s)[i]
+	*s = (*s)[:i]
+	return e
+}
+
+func inorderTraversalIterative(root *TreeNode) []int {
+	res := []int{}
+	if root == nil {
+		return res
+	}
+
+	stackNode := stack{root}
+	for !stackNode.isEmpty() {
+		r := stackNode.peek()
+		if r.Left == nil {
+			res = append(res, stackNode.pop().Val)
+			if r.Right != nil {
+				stackNode.push(r.Right)
+			}
+		}
+		if r.Left != nil {
+			stackNode.push(r.Left)
+			r.Left = nil
+		}
+	}
+	return res
+}
--- 10-tree/94_inorder_test.go
+++ 10-tree/94_inorder_test.go
@@ -48,11 +48,94 @@ func Test_inorderTraversal(t *testing.T) {
 			},
 			want: []int{1, 3, 2},
 		},
+		{
+			name: "many case 2",
+			args: args{
+				root: &TreeNode{
+					Val: 3,
+					Left: &TreeNode{
+						Val: 1,
+					},
+					Right: &TreeNode{
+						Val: 2,
+					},
+				},
+			},
+			want: []int{1, 3, 2},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := inorderTraversal(tt.args.root); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("inorderTraversal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_inorderTraversalIterative(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "zero",
+			args: args{
+				root: nil,
+			},
+			want: []int{},
+		},
+		{
+			name: "one case",
+			args: args{
+				root: &TreeNode{
+					Val:   1,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			want: []int{1},
+		},
+		{
+			name: "many case",
+			args: args{
+				root: &TreeNode{
+					Val:  1,
+					Left: nil,
+					Right: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val: 3,
+						},
+					},
+				},
+			},
+			want: []int{1, 3, 2},
+		},
+		{
+			name: "many case 2",
+			args: args{
+				root: &TreeNode{
+					Val: 3,
+					Left: &TreeNode{
+						Val: 1,
+					},
+					Right: &TreeNode{
+						Val: 2,
+					},
+				},
+			},
+			want: []int{1, 3, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := inorderTraversalIterative(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("inorderTraversalIterative() = %v, want %v", got, tt.want)
 			}
 		})
 	}
