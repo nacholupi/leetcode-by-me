62a4cae8b9eecf9ea458fd392815697917b337e9
nacholupi <nacholupi@gmail.com>
2022-05-19 16:16:57 -0300

    102. Binary Tree Level Order Traversal

---
+++ 11-tree/102-level.go
@@ -0,0 +1,40 @@
+package tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+
+	nodes := []*TreeNode{root}
+	res := [][]int{}
+
+	for len(nodes) != 0 {
+		var nInt []int
+		var newNodes []*TreeNode
+
+		for _, n := range nodes {
+
+			nInt = append(nInt, n.Val)
+
+			if n.Left != nil {
+				newNodes = append(newNodes, n.Left)
+			}
+
+			if n.Right != nil {
+				newNodes = append(newNodes, n.Right)
+			}
+		}
+		if len(nInt) > 0 {
+			res = append(res, nInt)
+		}
+		nodes = newNodes
+	}
+
+	return res
+}
